829fd66e2266f7d54031c8b3f4f516cf9d16ca7b
Pankaj-Lohani <pankajlohani2016@gmail.com>
2025-02-25 15:35:13 +0530

    new

--- 182. Duplicate Emails/code.sql
+++ 182. Duplicate Emails/code.sql
@@ -4,4 +4,4 @@ GROUP BY email
 HAVING COUNT(email) > 1;
 -- Note :-  GROUP BY email : Groups rows by the email column.
 -- COUNT(email) > 1:  Filters only those emails that appear more than once.
--- SELECT email : Retrieves the duplicate emails.
+-- SELECT email : Retrieves the duplicate
